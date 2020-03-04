964ec4bc441ff705d57ee015475e0febd0bccc57
mesneym <mesney.mesney@gmail.com>
2020-03-04 07:18:58 -0500

    Rectified some errors.Need to fix solution

--- conf_space.py
+++ conf_space.py
@@ -1,5 +1,8 @@
 import pygame
 import numpy as np
+from dijkstra_point import *
+
+
 
 
 #################################
@@ -17,7 +20,9 @@ blue = (0,0,255)
 res = 3 #resolution of grid
 scale = 3 #scale of grid
 
-gameDisplay = pygame.display.set_mode((300*scale,200*scale))
+size_x = math.ceil(300)
+size_y = math.ceil(200)
+gameDisplay = pygame.display.set_mode((size_x*scale,size_y*scale))
 
 
 #####################################
@@ -26,27 +31,79 @@ gameDisplay = pygame.display.set_mode((300*scale,200*scale))
 circlePts = [255,50,25]
 
 polygonPts =  np.array([[20,120],[25,185],[75,185],[100,150],[75,120],[50,150],[20,120]])
-polygonPts[:,1] = (200 - polygonPts[:,1])
+polygonPts[:,1] = (size_y - polygonPts[:,1])
 
 rhombusPts = np.array([[225,10],[200,25],[225,40],[250,25]])
-rhombusPts[:,1] = (200 - rhombusPts[:,1])
+rhombusPts[:,1] = (size_y - rhombusPts[:,1])
 
 ellipsePts = np.array([120,80,80,40])
 
 rectPts = np.array([[95,30],[20,30],[20,40],[95,40]])
-rectPts[:,1] = (200 - rectPts[:,1]) #To be modified
-
+rectPts[:,1] = (size_y - rectPts[:,1]) #To be modified
 
 pygame.draw.circle(gameDisplay, red, (circlePts[0]*scale,circlePts[1]*scale), circlePts[2]*scale)
 pygame.draw.polygon(gameDisplay,red,scale*polygonPts)
 pygame.draw.polygon(gameDisplay,red,scale*rhombusPts)
 pygame.draw.polygon(gameDisplay,red,scale*rectPts)
 pygame.draw.ellipse(gameDisplay,red,scale*ellipsePts)
+pygame.display.flip()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
 
-    pygame.display.update()
+##############################################
+#     Explore Nodes
+##############################################
+A = np.array([[1,0],[0,1],[-1,0],[0,-1],[1,1],[-1,-1],[1,-1],[-1,1]])
+costIncrement = [1.0,1.0,1.0,1.0,math.sqrt(2),math.sqrt(2),math.sqrt(2),math.sqrt(2)]
+
+s1 = 0
+s2 = 0
+g1 = 50
+g2 = 140
+
+nodesExplored = {}
+q = deque()
+startPosition = np.round((np.array([s1,s2]))/res)
+goalPosition = np.round((np.array([g1,g2]))/res)
+
+if(not isSafe(startPosition) and not isSafe(goalPosition)):
+    print("Start or goal position must be in a valid workspace")
+
+else:
+    success,solution = generatePath(q,A,startPosition,goalPosition,costIncrement,nodesExplored)
+
+
+    #############################################
+    #     Drawing 
+    #############################################
+    draw = True
+    while draw:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        
+        #draw nodesExplored
+        for s in nodesExplored:
+            pt = nodesExplored[s].state
+            x,y = pt*scale*res
+
+            #draw explored nodes
+            pygame.draw.rect(gameDisplay,white,(x,y,res*scale,res*scale))
+
+            #draw start and goal locations
+            pygame.draw.rect(gameDisplay,blue,(startPosition[0]*res*scale,startPosition[1]*res*scale,res*scale,res*scale))
+            pygame.draw.rect(gameDisplay,blue,(goalPosition[0]*res*scale,goalPosition[1]*res*scale,res*scale,res*scale))
+            pygame.display.flip()
+       
+
+        # draw solution path
+        for i in range(len(solution)-1,-1,-1):
+            pt = solution[i]
+            x,y = pt*scale*res
+            pygame.draw.rect(gameDisplay,green,(x,y,res*scale,res*scale))
+            pygame.display.flip()
+        pygame.time.delay(1000)
+        draw = False
+    pygame.quit()
+
+
--- dijkstra_point.py
+++ dijkstra_point.py
@@ -5,7 +5,7 @@ import math
 
 
 ######################################
-#          Workspace
+#          Workspace=
 ######################################
 def isValidWorkspace(pt,r = 3): #To be modified
     circle = (pt[0] - math.floor(225/r))**2 + (pt[1]-math.floor(150/r))**2 
@@ -20,55 +20,50 @@ def isSafe(newState,r=3):
     col = math.floor(300/r)
     row = math.floor(200/r)
 
-    if(newState[0]< 0 or newState[0]>row or newState[1]<0 or newState[1]>1):
+    if(newState[0]< 0 or newState[0]>row or newState[1]<0 or newState[1]>col):
         return False
-    return isValidWorkspace(newState,3)
+    return isValidWorkspace(newState,r)
 
+#prints solution path 
 def printPath(node):
+    l = []
     current = node
     while(current):
-       print(current.state)
+       l.append(current.state)
        current = current.parent
+    return l
+
 
 
 #generates optimal path for robot
-def generatePath(q,visited,A,startPosition,goalPosition,costIncrement,nodesExplored):
+def generatePath(q,A,startPosition,goalPosition,costIncrement,nodesExplored,res=3):
     key = str(startPosition[0]) + str(startPosition[1])
     root = Node(startPosition,0.0,None)
     nodesExplored[key] = root
-    visited.add(key)
     q.appendleft(root)
     
     while(len(q)>0):
         currentNode = q.pop()
         if((currentNode.state == goalPosition).all()):
-            printPath(currentNode)
-            break
-       
-        print("=====================")
-        print(currentNode.state)
-        print(currentNode.parent)
-        # if(currentNode.parent):
-            # print(currentNode.parent.state)
-        print("======================")
-        print(" ")
+            sol = printPath(currentNode)
+            return [True,sol]
+
         for i in range(8): 
             newState = A[i,:] + currentNode.state 
             s = str(newState[0])+str(newState[1])
 
-            if(s not in visited and isSafe(newState)):
-                visited.add(s)
-                newCost = currentNode.cost + costIncrement[i]
-                newNode = Node(newState,newCost,Node)
-                nodesExplored[s] = newNode
-                q.append(newNode)
+            if(s not in nodesExplored):
+                if(isSafe(newState,res)):
+                    newCost = currentNode.cost + costIncrement[i]
+                    newNode = Node(newState,newCost,currentNode)
+                    nodesExplored[s] = newNode
+                    q.appendleft(newNode)
 
             else:
-                if(s in key and nodesExplored[s].cost > currentNode.cost + costIncrement[i]):
+                if(nodesExplored[s].cost > currentNode.cost + costIncrement[i]):
                     nodesExplored[s].cost = currentNode.cost + costIncrement[i]
                     nodesExplored[s].parent = currentNode
-
-    return False
+    return  [False,None] 
 
 
 if __name__ == "__main__":
@@ -76,12 +71,11 @@ if __name__ == "__main__":
     A = np.array([[1,0],[0,1],[-1,0],[0,-1],[1,1],[-1,-1],[1,-1],[-1,1]])
     costIncrement = [1.0,1.0,1.0,1.0,math.sqrt(2),math.sqrt(2),math.sqrt(2),math.sqrt(2)]
 
-    visited = set()
     nodesExplored = {}
     q = deque()
-    startPosition = np.array([0,0])
+    startPosition = np.array([12,2])
     goalPosition = np.array([5,5])
-    print(generatePath(q,visited,A,startPosition,goalPosition,costIncrement,nodesExplored))
+    print(generatePath(q,A,startPosition,goalPosition,costIncrement,nodesExplored))
     
 
 
